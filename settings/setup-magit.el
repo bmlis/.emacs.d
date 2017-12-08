f2012ea0e100a44b968e64cd5eb83d1f0b94ec7a
Bartlomiej Lisiecki <b.m.lisiecki@gmail.com>
2017-12-08 15:27:45 +0100

    Use git-gutter instead of vc

--- settings/setup-magit.el
+++ settings/setup-magit.el
@@ -12,4 +12,18 @@
   (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
   (global-diff-hl-mode))
 
+(use-package git-gutter
+  :ensure t
+  :init
+  (custom-set-variables
+   '(git-gutter:modified-sign " "))
+  (set-face-background 'git-gutter:modified "#90CAF9")
+  (set-face-foreground 'git-gutter:modified "#E3F2FD")
+  (set-face-background 'git-gutter:added "#CDDC39")
+  (set-face-foreground 'git-gutter:added "#F0F4C3")
+  (set-face-background 'git-gutter:deleted "#FF5722")
+  (set-face-foreground 'git-gutter:deleted "#FF9E80")
+  (setq vc-handled-backends ())
+  (global-git-gutter-mode t))
+
 (provide 'setup-magit)
