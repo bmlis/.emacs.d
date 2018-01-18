8fc2ea58c95c4b642a4d78a3d1a34e4ea46a123a
Bartlomiej Lisiecki <bartlomiej.lisiecki@pragmaticcoders.com>
2018-01-18 14:48:03 +0100

    Hight theme

--- init.el
+++ init.el
@@ -21,7 +21,8 @@
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+      `((".*" ,temporary-file-directory t))
+(load-theme 'my-tao t)
 
 (global-unset-key (kbd "C-z"))
 
@@ -39,10 +40,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- (setq vc-handled-backends nil)
  '(custom-safe-themes
    (quote
     ("bfb2b4217c1a71c1c4201a66ec52f33c4fab30bff5306dd9c3f8524ac2e8e7c7" "2ed1c05f204f4a6650900541dd3cea0f06a33297faaa1c9e048a84e77222e8da" "63dee1f4324dd686e79ebae44bb575d5da82950b68506ba7953e4dc83cb837be" "d19b6cb50f86a0460826bde9a8d491e8ca4bac66f632c38ce081b0155ca36f01" "3fc999d3bad959dd579f264da93b8736a8aef16ae106bed8403b00cf2b4723ed" "f5ea697467b84a681fb8f984789198090975de04c42fda067c3560d3d3d62d67" "e5821980bd8d5c29741f9cd2a164b69f4419bb7e8e11f71b92371218ae3325f2" "03720f7b2e15c6d967f6e8865f1f1d365e1834c29bd52b43063f829095753a20" "dccee79c92557e4f62f195d1701fed3da22334e6f084ea319405e6981f771ef2" "9899b7fb8c8fd66c27dad25d19745c2edb76aa6e4bf6fc223c77fd729b81ee22" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(nil nil t)
  '(package-selected-packages
    (quote
     (git-gutter 0blayout ace-window ranger evil use-package scpaste smex magit find-file-in-project ido-ubiquitous idle-highlight-mode paredit better-defaults))))
