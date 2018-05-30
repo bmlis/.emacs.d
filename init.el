d24958044992427809b2830bbf5dd38dfb483142
Bartlomiej Lisiecki <bartlomiej.lisiecki@rumblefish.pl>
2018-05-30 12:26:02 +0200

    White tao theme

--- init.el
+++ init.el
@@ -21,7 +21,6 @@
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
-(load-theme 'my-tao t)
 
 (setq vc-handled-backends ())
 
